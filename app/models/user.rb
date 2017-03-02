85fc0f5c0ee8f6623ecdfca261af1b41290d7813
chcholman <caitlin.holman@gmail.com>
2017-03-01 22:29:53 -0500

    Temp fix to get beta up and running

--- app/models/user.rb
+++ app/models/user.rb
@@ -113,8 +113,7 @@ class User < ActiveRecord::Base
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :username, presence: true,
-                    length: { maximum: 50 },
-                    uniqueness: { case_sensitive: false }
+                    length: { maximum: 50 }
   validates :email, presence: true,
                     format: { with: email_regex },
                     uniqueness: { case_sensitive: false }
