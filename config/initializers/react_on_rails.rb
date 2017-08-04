556a4a4854c97a873a599305603e623e3dc230eb
Jonathan Gabel <hello@jonathangabel.com>
2017-08-04 13:03:13 -0400

    test with specific node version

--- config/initializers/react_on_rails.rb
+++ config/initializers/react_on_rails.rb
@@ -18,7 +18,10 @@ ReactOnRails.configure do |config|
   # If you are using the ReactOnRails::TestHelper.configure_rspec_to_compile_assets(config)
   # with rspec then this controls what yarn command is run
   # to automatically refresh your webpack assets on every test run.
-  config.npm_build_test_command = "yarn run build:test"
+
+  # GRADECRAFT CI NOTE: `yarn &&` is added here for Codeship. We should add `yarn install`
+  # to build commands within codeship, to avoid running locally, and remove.
+  config.npm_build_test_command = "yarn && yarn run build:test"
 
   # This configures the script to run to build the production assets by webpack. Set this to nil
   # if you don't want react_on_rails building this file for you.
