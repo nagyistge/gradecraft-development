a55b09e53f8be9f74fe631f4f15b9ff885eedfe7
Jon Yu <jonathoy@umich.edu>
2017-08-07 12:11:43 -0400

    Add course_id rows, ability to enable objectives

--- db/schema.rb
+++ db/schema.rb
@@ -530,6 +530,7 @@ ActiveRecord::Schema.define(version: 20171005150420) do
   end
 
   create_table "learning_objective_levels", force: :cascade do |t|
+    t.integer "course_id"
     t.integer "objective_id",  null: false
     t.string  "name",          null: false
     t.string  "description"
@@ -537,6 +538,7 @@ ActiveRecord::Schema.define(version: 20171005150420) do
   end
 
   create_table "learning_objective_links", force: :cascade do |t|
+    t.integer "course_id"
     t.integer "objective_id",                     null: false
     t.string  "learning_objective_linkable_type"
     t.integer "learning_objective_linkable_id"
@@ -544,6 +546,7 @@ ActiveRecord::Schema.define(version: 20171005150420) do
   end
 
   create_table "learning_objective_outcomes", force: :cascade do |t|
+    t.integer  "course_id"
     t.integer  "objective_id"
     t.integer  "objective_level_id",                 null: false
     t.datetime "assessed_at",                        null: false
