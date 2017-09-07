c7a62cf394ba0ff3ae22549a493a763b1481c0f4
Jon Yu <jonathoy@umich.edu>
2017-09-07 14:27:00 -0400

    Add columns for managing points, visibility
    .. of learning objectives on a course-by-course basis

--- db/schema.rb
+++ db/schema.rb
@@ -332,6 +332,8 @@ ActiveRecord::Schema.define(version: 20171005150420) do
     t.text     "dashboard_message"
     t.integer  "learning_objective_term",                                 default: 0,                            null: false
     t.boolean  "has_learning_objectives",                                 default: false,                        null: false
+    t.boolean  "objectives_award_points",                                 default: false,                        null: false
+    t.boolean  "always_show_objectives",                                  default: false,                        null: false
     t.index ["institution_id"], name: "index_courses_on_institution_id", using: :btree
   end
 
